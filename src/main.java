d0d4fbba91574836eaf2094afb9401b4ddc8f848
jfitz1382 <jfitz1382@gmail.com>
2022-07-26 22:37:41 -0400

    image commit

--- src/main.java
+++ src/main.java
@@ -1,9 +1,22 @@
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		JFrame frame = new JFrame();
+		frame.setTitle("Game");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+		
+		ImageIcon image = new ImageIcon("img/menubkg.jpg");
+		frame.setIconImage(image.getImage());
+		frame.getContentPane().setBackground(new Color(0xD1692E));
 	}
-
+	
 }
