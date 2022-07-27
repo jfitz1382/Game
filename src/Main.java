481d97ba5324102d918b2b87c30d32137a05b8cc
root <root@joshua-inspiron137359>
2022-07-27 14:00:14 -0400

    i think this is the right file

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+		frame.setTitle("Game");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+		
+		ImageIcon image = new ImageIcon("img/menubkg.jpg");
+		frame.setIconImage(image.getImage());
+		//frame.getContentPane().setBackground(new Color(0xD1692E));
+
+	}
+
+}
